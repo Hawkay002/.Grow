3e7a8b1202dcb2a2ba56a4ddbda4a99653377716
Shovith Debnath <119169431+Hawkay002@users.noreply.github.com>
2026-04-10 00:11:19 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,7 @@ export default {
       keyframes: {
         fadeInUp: {
           '0%': { opacity: '0', transform: 'translateY(10px)' },
-          '100%': { opacity: '1', transform: 'translateY(0)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' }, 
         }
       }
     },
