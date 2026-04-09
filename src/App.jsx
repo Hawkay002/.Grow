4d1d13ade2c91186251c59bdd0941e63ea242e6e
Shovith Debnath <119169431+Hawkay002@users.noreply.github.com>
2026-04-09 19:59:01 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
 import QrScanner from './pages/QrScanner';
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        {/* The Creator Journey */}
-        <Route path="/" element={<Dashboard />} />
-        
-        {/* The Scanner Journey (Dynamic route expecting a unique ID) */}
-        <Route path="/qr/:id" element={<QrScanner />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/qr/:id" element={<QrScanner />} />
+          
+          {/* Protected Route (We will lock this down later) */}
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
